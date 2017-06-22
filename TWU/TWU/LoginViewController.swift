e0634eefbfabafb6062d949287125bcf5bcbbbec
ATran.dev <tranxuanai9x@gmail.com>
2017-06-22 08:38:26 +0700

    remove libs Material

---
+++ TWU/TWU/LoginViewController.swift
@@ -0,0 +1,24 @@
+//
+//  LoginViewController.swift
+//  TWU
+//
+//  Created by ATran.dev on 6/22/17.
+//  Copyright © 2017 ATGroup. All rights reserved.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+}
